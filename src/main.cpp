039203e8ecccabdc3e56b61f8355ca7c8df7c1ce
Pierce Primm, Sr <pierce@primm.cc>
2018-11-18 17:01:25 -0600

    1st pass as complete state machine and byte decoding

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #define DT_T1 300
 #define DT_T2 600
 #define DT_T3 1600
-#define BYTE_COUNT 32
+#define BYTE_COUNT 10
 
 
 /**
@@ -19,24 +19,24 @@
 volatile unsigned char state = IDLE;
 volatile unsigned char isrBytes[BYTE_COUNT];
 volatile unsigned char byteIndex = 0;
-volatile unsigned char bitIndex = 0;
-volatile unsigned char input = LOW;
 volatile int bitCount = 0;
-volatile int loopCount = 0;
 /*******************************************************************************
  * ISR for input change event
 *******************************************************************************/
+volatile unsigned char input;
+volatile unsigned char bitLevel;
 static unsigned long oldTime = 0;
 static unsigned long newTime;
 static unsigned long tDiff;
+volatile unsigned char bitIndex = 0;
 void inputChange()
 {
   input = digitalRead(INPUT_PIN);
+  bitLevel = input == LOW ? 0x01 : 0x00;
   newTime = micros();
   tDiff = newTime - oldTime;
   oldTime = newTime;
-  ++loopCount;
-  if (tDiff < DT_T1) {
+  if (tDiff < DT_T1 && input == LOW) {
     state = TEMP;
   }
   switch (state)
@@ -45,6 +45,7 @@ void inputChange()
       if (tDiff > DT_T3 && input == HIGH) {
         state = START;
         bitCount = 0;
+        byteIndex = 0;
       }
       break;
     case START:
@@ -53,10 +54,16 @@ void inputChange()
         break;
       }
       ++bitCount;
+      byteIndex = bitCount / 8;
+      bitIndex = bitCount % 8;
+      isrBytes[byteIndex] |= bitLevel << bitIndex;
       break;
     case END:
       ++bitCount;
       state = START;
+      byteIndex = bitCount / 8;
+      bitIndex = bitCount % 8;
+      isrBytes[byteIndex] |= bitLevel << bitIndex;
       break;
     case TEMP:
       break;
@@ -65,19 +72,33 @@ void inputChange()
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("Running");
   pinMode(INPUT_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(INPUT_PIN), inputChange, CHANGE);
 }
 
+void printButton()
+{
+  Serial.print(bitCount,DEC);
+  Serial.print(" ");
+  Serial.print(byteIndex,DEC);
+  Serial.print(" ");
+  for (int i = 0;i <= byteIndex && i < BYTE_COUNT;++i) {
+    Serial.print(isrBytes[i],HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
 void loop() 
 {
-  Serial.print(loopCount,DEC);
-  Serial.print("   ");
-  Serial.println(bitCount,DEC);
-  state = IDLE;
-  bitCount = 0;
-  loopCount = 0;
-  delay(2000);
+  if (state == TEMP) {
+    if (bitCount == 77) {
+      printButton();
+    }
+    memset((void*)(isrBytes),0,sizeof(isrBytes));
+    state = IDLE;
+  }
+  delay(1);
 }
