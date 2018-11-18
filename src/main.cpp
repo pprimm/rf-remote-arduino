33f40d53257d4899d639cd32f8c416192ecf375b
Pierce Primm, Sr <pierce@primm.cc>
2018-11-18 15:20:03 -0600

    Initial state machine counting bits; unable to use static in ISR

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,64 @@
 #include <Arduino.h>
 
-#define RESET 0
-#define ARM 1
-#define CAPTURE 2
+#define INPUT_PIN 2
+// ISR States
+#define IDLE 0
+#define START 1
+#define END 2
+#define TEMP 3
+// Bit stream timings
+#define DT_T1 300
+#define DT_T2 600
+#define DT_T3 1600
+#define BYTE_COUNT 32
 
-volatile unsigned long count  = 0;
-volatile unsigned long newTime;
-volatile unsigned long oldTime;
-volatile unsigned long diff = 0;
-volatile unsigned short state = RESET;
 
 /**
- * 
-  if (!arm) return;
-  if (once) return;
-  once = true;
-  ++count;
-  newTime = micros();
-  diff = newTime - oldTime;
-  oldTime = newTime;
-  if (diff < min) {
-    min = diff;
-  }
- * 
-**/
-
-void counter()
+ * Shared between ISR and loop()
+ */
+volatile unsigned char state = IDLE;
+volatile unsigned char isrBytes[BYTE_COUNT];
+volatile unsigned char byteIndex = 0;
+volatile unsigned char bitIndex = 0;
+volatile unsigned char input = LOW;
+volatile int bitCount = 0;
+volatile int loopCount = 0;
+/*******************************************************************************
+ * ISR for input change event
+*******************************************************************************/
+static unsigned long oldTime = 0;
+static unsigned long newTime;
+static unsigned long tDiff;
+void inputChange()
 {
+  input = digitalRead(INPUT_PIN);
   newTime = micros();
+  tDiff = newTime - oldTime;
+  oldTime = newTime;
+  ++loopCount;
+  if (tDiff < DT_T1) {
+    state = TEMP;
+  }
   switch (state)
   {
-    case ARM:
-      oldTime = micros();
-      state = CAPTURE;
+    case IDLE:
+      if (tDiff > DT_T3 && input == HIGH) {
+        state = START;
+        bitCount = 0;
+      }
       break;
-    case CAPTURE:
-      diff = newTime - oldTime;
-      state = RESET;
+    case START:
+      if (tDiff < DT_T2) {
+        state = END;
+        break;
+      }
+      ++bitCount;
+      break;
+    case END:
+      ++bitCount;
+      state = START;
+      break;
+    case TEMP:
       break;
   }
 }
@@ -45,15 +67,17 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println("Running");
-  attachInterrupt(digitalPinToInterrupt(2), counter, CHANGE);
+  pinMode(INPUT_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(INPUT_PIN), inputChange, CHANGE);
 }
 
 void loop() 
 {
-  Serial.print(count,DEC);
-  Serial.print("  ");
-  Serial.println(diff,DEC);
-  state = ARM;
-  diff = 0;
-  delay(1000);
+  Serial.print(loopCount,DEC);
+  Serial.print("   ");
+  Serial.println(bitCount,DEC);
+  state = IDLE;
+  bitCount = 0;
+  loopCount = 0;
+  delay(2000);
 }
